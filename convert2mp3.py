f1713ed9210496299241db6999bb04180244b8c1
lyraxvincent <njongevincent@gmail.com>
2021-11-09 10:57:09 +0300

    correct outputting .isfile(directory)

--- convert2mp3.py
+++ convert2mp3.py
@@ -36,7 +36,7 @@ for directory in os.listdir("."):
 		if (audio_format != "mp3") and (audio_format != "py"):
 			try:
 				m4a_audio = AudioSegment.from_file("{}".format(str(directory)), str(audio_format))
-				print(f"Converting [{f}]")
+				print(f"Converting [{directory}]")
 				m4a_audio.export("{}".format(str(directory).split(str(audio_format))[0]+".mp3"), format="mp3")
 				# delete previous format
 				os.remove(directory)
